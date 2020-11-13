45f39b14a01455f0b379e746bc306b18fb83de0f
Łukasz Miłkowski <lukanus@uaznia.net>
2020-11-13 15:22:21 +0100

    main and makefile

---
+++ cmd/skale-indexer/config/config.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+var (
+	Name      = "cosmos-worker"
+	Version   string
+	GitSHA    string
+	Timestamp string
+)
+
+const (
+	modeDevelopment = "development"
+	modeProduction  = "production"
+)
+
+type Config struct {
+	DatabaseDSN string
+}
+
+// IdentityString returns the full app version string
+func IdentityString() string {
+
+	t, err := strconv.Atoi(Timestamp)
+	timestamp := Timestamp
+	if err == nil {
+		timestamp = time.Unix(int64(t), 0).String()
+	}
+	return fmt.Sprintf(
+		"%s %s (git: %s) - built at %s",
+		Name,
+		Version,
+		GitSHA,
+		timestamp,
+	)
+}
+
+// FromFile reads the config from a file
+func FromFile(path string, config *Config) error {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(data, config)
+}
+
+// FromEnv reads the config from environment variables
+func FromEnv(config *Config) error {
+	return envconfig.Process("", config)
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/figment-networks/skale-indexer
+
+go 1.15
+
+require (
+	github.com/bearcherian/rollzap v1.0.2
+	github.com/ethereum/go-ethereum v1.9.23
+	github.com/figment-networks/indexing-engine v0.1.14
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/rollbar/rollbar-go v1.2.0
+	go.uber.org/zap v1.16.0
+)
