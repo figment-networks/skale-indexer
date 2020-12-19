11d0fd2d1c5c9a8d1bdc96884b7bb9017565d964
eesmerdag <emiresmerdag@gmail.com>
2020-12-19 03:33:19 +0300

    test

--- client/transport/webapi/response.go
+++ client/transport/webapi/response.go
@@ -47,20 +47,20 @@ type NodeAPI struct {
 type ValidatorAPI struct {
 	ValidatorID             *big.Int       `json:"validator_id"`
 	Name                    string         `json:"name"`
-	ValidatorAddress        common.Address `json:"validator_address"`
-	RequestedAddress        common.Address `json:"requested_address"`
-	Description             string         `json:"description"`
-	FeeRate                 *big.Int       `json:"fee_rate"`
+	//ValidatorAddress        common.Address `json:"validator_address"`
+	//RequestedAddress        common.Address `json:"requested_address"`
+	//Description             string         `json:"description"`
+	//FeeRate                 *big.Int       `json:"fee_rate"`
 	RegistrationTime        time.Time      `json:"registration_time"`
 	//MinimumDelegationAmount *big.Int       `json:"minimum_delegation_amount"`
-	AcceptNewRequests       bool           `json:"accept_new_requests"`
-	Authorized              bool           `json:"authorized"`
-	Active                  bool           `json:"active"`
+	//AcceptNewRequests       bool           `json:"accept_new_requests"`
+	//Authorized              bool           `json:"authorized"`
+	//Active                  bool           `json:"active"`
 	ActiveNodes             int            `json:"active_nodes"`
-	LinkedNodes             int            `json:"linked_nodes"`
-	Staked                  uint64         `json:"staked"`
-	Pending                 uint64         `json:"pending"`
-	Rewards                 uint64         `json:"rewards"`
+	//LinkedNodes             int            `json:"linked_nodes"`
+	//Staked                  uint64         `json:"staked"`
+	//Pending                 uint64         `json:"pending"`
+	//Rewards                 uint64         `json:"rewards"`
 }
 
 // TODO: change api response with this
