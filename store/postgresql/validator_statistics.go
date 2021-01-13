fd97d2da4dd4a4f71c7aeaa86ee372b366eaa5b7
eesmerdag <emiresmerdag@gmail.com>
2021-01-13 17:37:58 +0300

    using COALESCE

--- store/postgresql/validator_statistics.go
+++ store/postgresql/validator_statistics.go
@@ -119,7 +119,7 @@ func (d *Driver) CalculateTotalStake(ctx context.Context, params structs.Validat
 		return err
 	}
 
-	res, err := tx.ExecContext(ctx, `INSERT INTO validator_statistics (validator_id, block_height, statistic_type, amount)
+	_, err = tx.ExecContext(ctx, `INSERT INTO validator_statistics (validator_id, block_height, statistic_type, amount)
 										SELECT $3, $1, $2, sum(t1.amount) AS amount
 									FROM
 											( SELECT DISTINCT ON (delegation_id) validator_id, delegation_id, block_height, state, amount
@@ -138,26 +138,15 @@ func (d *Driver) CalculateTotalStake(ctx context.Context, params structs.Validat
 		return err
 	}
 
-	affectedRows, err := res.RowsAffected()
-	if err != nil {
-		if rollbackErr := tx.Rollback(); rollbackErr != nil {
-			return rollbackErr
-		}
-		return err
-	}
-
-	if affectedRows != 0 {
-		_, err = tx.Exec(`UPDATE validators
+	_, err = tx.Exec(`UPDATE validators
 						SET staked = (
-							 	SELECT amount
+							 	SELECT COALESCE((SELECT DISTINCT ON (validator_id, statistic_type) amount
 								 FROM validator_statistics
-								 WHERE validator_id = $1 AND statistic_type = $2 AND block_height = $3 )
+								 WHERE validator_id = $1 AND statistic_type = $2 
+								 ORDER BY validator_id, statistic_type, block_height DESC), 0))
 						WHERE validator_id = $4`,
-			params.ValidatorID, structs.ValidatorStatisticsTypeTotalStake, params.BlockHeight, params.ValidatorID)
-	} else {
-		_, err = tx.Exec(`UPDATE validators SET staked = 0 WHERE validator_id = $1`,
-			params.ValidatorID)
-	}
+		params.ValidatorID, structs.ValidatorStatisticsTypeTotalStake, params.ValidatorID)
+
 	if err != nil {
 		if rollbackErr := tx.Rollback(); rollbackErr != nil {
 			return rollbackErr
@@ -166,7 +155,6 @@ func (d *Driver) CalculateTotalStake(ctx context.Context, params structs.Validat
 	}
 
 	return tx.Commit()
-
 }
 
 func (d *Driver) CalculateActiveNodes(ctx context.Context, params structs.ValidatorStatisticsParams) error {
