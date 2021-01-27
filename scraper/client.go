a90d42faa34fbdd99c02cd85e0162adafa4faae4
eesmerdag <emiresmerdag@gmail.com>
2021-01-27 20:12:52 +0300

    change function signature

--- scraper/client.go
+++ scraper/client.go
@@ -20,7 +20,7 @@ type ActionManager interface {
 	GetImplementedContractNames() []string
 	GetBlockHeader(ctx context.Context, height *big.Int) (h *types.Header, err error)
 	AfterEventLog(ctx context.Context, c contract.ContractsContents, ce structs.ContractEvent) error
-	SyncForEndOfEpoch(ctx context.Context, c contract.ContractsContents)
+	SyncForEndOfEpoch(ctx context.Context, c contract.ContractsContents, currentBlock uint64) error
 }
 
 type EthereumAPI struct {
